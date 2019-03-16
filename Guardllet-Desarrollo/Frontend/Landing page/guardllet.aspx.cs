a2a04960f10cba6832fab449c04dbd9c3d2018fb
Josue Garcia <josue.it1999@gmail.com>
2019-03-15 18:34:29 -0600

    Mas

---
+++ Guardllet-Desarrollo/Frontend/Landing page/guardllet.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Guardllet_Desarrollo.Frontend.Landing_page
+{
+    public partial class guardllet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
